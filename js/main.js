a3258a9f35bf6df5da2baf8cd4b6009468a1f7dd
vinaydoddapaneni <vinaydoddapaneni@gmail.com>
2022-02-02 13:23:02 +0530

    added few colors surround of the div

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@
 function changeImg() {
     var ram = Math.ceil(Math.random() * 20)
     document.getElementById("img").src = `https://picsum.photos/200/300?random=${ram}`;
+    document.getElementById("body").style.backgroundColor = "black";
 }

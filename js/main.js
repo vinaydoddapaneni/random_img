25853e829da9f793d77d26c25f1ffdfe8a60e821
vinaydoddapaneni <vinaydoddapaneni@gmail.com>
2022-02-02 11:00:02 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// var ram = Math.ceil(Math.random() * 10)
+function changeImg() {
+    var ram = Math.ceil(Math.random() * 20)
+    document.getElementById("img").src = `https://picsum.photos/200/300?random=${ram}`;
+    console.log(ram);
+}

450edbadac5f01e99d889474934f5026d8d05446
vinaydoddapaneni <vinaydoddapaneni@gmail.com>
2022-02-02 19:03:46 +0530

    added few fun content in js

--- js/main.js
+++ js/main.js
@@ -2,5 +2,11 @@
 function changeImg() {
     var ram = Math.ceil(Math.random() * 20)
     document.getElementById("img").src = `https://picsum.photos/200/300?random=${ram}`;
+    document.getElementById("para").innerHTML = "Don't Click on Image.";
     document.getElementById("body").style.backgroundColor = "black";
+}
+
+function changeContent(){
+    document.getElementById("img").src = '#'
+    document.getElementById("para").innerHTML = "I told you, don't click on the Image."
 }
